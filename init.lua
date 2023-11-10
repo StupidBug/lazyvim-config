c17ff427ec2b512ec114c74fe1070b8e7dd649f2
StupidBug <2228561590@qq.com>
2023-11-10 17:28:07 +0800

    fix: input method switch not work

--- init.lua
+++ init.lua
@@ -4,12 +4,12 @@ require("config.lazy")
 -- Switch input method when enter or leave insert mode
 vim.api.nvim_create_autocmd("InsertLeave", {
     callback = function()
-        vim.cmd(":silent:!im-select com.apple.keylayout.ABC")
+        vim.cmd(":silent:!macism com.apple.keylayout.ABC")
     end,
 })
 vim.api.nvim_create_autocmd("InsertEnter", {
     callback = function()
-        vim.cmd(":silent:!im-select com.sogou.inputmethod.sogou.pinyin")
+        vim.cmd(":silent:!macism com.sogou.inputmethod.sogou.pinyin")
     end,
 })
 
